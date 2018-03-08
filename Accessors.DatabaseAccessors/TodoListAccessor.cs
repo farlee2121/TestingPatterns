6125eae68024d8a3812aa271484a79c4652fce11
farlee2121 <2847259+farlee2121@users.noreply.github.com>
2018-03-08 17:47:24 -0600

    fill in accessor interface methods necessary to write managers

--- Accessors.DatabaseAccessors/TodoListAccessor.cs
+++ Accessors.DatabaseAccessors/TodoListAccessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.DataContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,35 @@ namespace Accessors.DatabaseAccessors
 {
     public interface ITodoListAccessor
     {
+        TodoList GetTodoList(Guid id);
 
+        IEnumerable<TodoList> GetTodoListsForUser(Guid userId);
+
+        SaveResult<TodoList> SaveTodoList(TodoList todoList);
+
+        DeleteResult DeleteTodoList(Guid id);
     }
 
     class TodoListAccessor : ITodoListAccessor
     {
+        public DeleteResult DeleteTodoList(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoList GetTodoList(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoList> GetTodoListsForUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SaveResult<TodoList> SaveTodoList(TodoList todoList)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Tests.ManagerTests/TodoCRUDManagerIntegrationTests.cs
+++ Tests.ManagerTests/TodoCRUDManagerIntegrationTests.cs
@@ -18,6 +18,13 @@ namespace Tests.ManagerTests
         // However, it causes duplicate names and both integration and 
         // unit runs will be directed to the inherited class, making it unclear which test is which
 
+        // The downside of this method is that it requires manual maintenance 
+        // Could explore the possibility of reflecting over a class's methods and raising an exception
+        // that highlights the failed method with a reason
+
+        // Another alternative would be looking at generating a code file at build time with a plugin
+        // It would be a pretty simple plugin with simple emitted code
+
         TodoCRUDManagerTests unitTests = new TodoCRUDManagerTests();
 
         [TestInitialize]
