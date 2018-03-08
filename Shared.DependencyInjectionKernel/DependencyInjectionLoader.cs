9ef97124cbb38372aced1f3d3316bdc5db90e2f6
farlee2121 <2847259+farlee2121@users.noreply.github.com>
2018-03-08 12:27:38 -0600

    Add an example of intergration/unit test reuse

---
+++ Shared.DependencyInjectionKernel/DependencyInjectionLoader.cs
@@ -0,0 +1,27 @@
+﻿using Accessors.DatabaseAccessors;
+using Managers.LazyCollectionOfAllManagers;
+using Ninject;
+using Ninject.Modules;
+using System.Collections.Generic;
+
+namespace Shared.DependencyInjectionKernel
+{
+    public static class DependencyInjectionLoader
+    {
+        // NOTE: This class centrallizes dependency injection configuration and removes the need to directly
+        // reference dependency projects. You can still instantiate a subset of dependencies if needed, but this
+        // centrallizes the usual concern of wanting all the full stack.
+        // This is only useful if you have multiple clients. In this case, our integration test are all effectively clients.
+
+        public static INinjectModule[] Modules = new INinjectModule[]
+        {
+            new LazyManagerLoader(),
+            new DatabaseAccessorLoader(),
+        };
+        public static IKernel BuildKernel()
+        {
+            IKernel kernel = new StandardKernel(Modules);
+            return kernel;
+        }
+    }
+}
