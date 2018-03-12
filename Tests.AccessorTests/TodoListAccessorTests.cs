731ff6a8ffceec9cc59f43a18d2aa2d40eed7dbc
farlee2121 <2847259+farlee2121@users.noreply.github.com>
2018-03-11 22:39:48 -0500

    Unit tests for TodoListAccessor

--- Tests.AccessorTests/TodoListAccessorTests.cs
+++ Tests.AccessorTests/TodoListAccessorTests.cs
@@ -1,5 +1,7 @@
 ﻿using Accessors.DatabaseAccessors;
+using DeepEqual.Syntax;
 using NUnit.Framework;
+using Shared.DataContracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,67 @@ namespace Tests.AccessorTests
         [Test]
         public void GetTodoList()
         {
+            TodoList expectedTodoList = dataPrep.TodoLists.Create();
 
+            TodoList actualTodoList = accessor.GetTodoList(expectedTodoList.Id);
+
+            expectedTodoList.ShouldDeepEqual(actualTodoList);
+        }
+
+        [Test]
+        public void GetTodoListsForUser()
+        {
+            User user = dataPrep.Users.Create();
+            IEnumerable<TodoList> expectedTodoListList = dataPrep.TodoLists.CreateManyForUser(5, user);
+
+            IEnumerable<TodoList> actualTodoListList = accessor.GetTodoListsForUser(user.Id);
+
+            expectedTodoListList.ShouldDeepEqual(actualTodoListList);
+        }
+
+        [Test]
+        public void SaveTodoList_Update()
+        {
+            TodoList expectedTodoList = dataPrep.TodoLists.Create();
+            expectedTodoList.Title = Guid.NewGuid().ToString();
+
+            SaveResult<TodoList> saveResult = accessor.SaveTodoList(expectedTodoList);
+            TodoList actualTodoList = saveResult.Result;
+
+            Assert.IsTrue(saveResult.Success);
+            expectedTodoList.ShouldDeepEqual(actualTodoList);
+        }
+
+        [Test]
+        public void SaveTodoList_Create()
+        {
+            User user = dataPrep.Users.Create();
+            TodoList newTodoList = new TodoList()
+            {
+                UserId = user.Id,
+                Title = Guid.NewGuid().ToString(),
+            };
+
+            SaveResult<TodoList> saveResult = accessor.SaveTodoList(newTodoList);
+            TodoList actualTodoList = saveResult.Result;
+
+            Assert.IsNotNull(actualTodoList.Id);
+            newTodoList.WithDeepEqual(actualTodoList).IgnoreSourceProperty(tl => tl.Id);
+        }
+
+        [Test]
+        public void DeleteTodoList()
+        {
+            TodoList expectedTodoList = dataPrep.TodoLists.Create();
+
+            // act
+            DeleteResult deleteResult = accessor.DeleteTodoList(expectedTodoList.Id);
+
+            //assert
+            Assert.IsTrue(deleteResult.Success);
+
+            TodoList retrievableTodoList = accessor.GetTodoList(expectedTodoList.Id);
+            Assert.IsNull(retrievableTodoList);
         }
     }
 }
