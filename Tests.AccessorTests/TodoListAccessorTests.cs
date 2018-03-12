fb137dae49a48baeebe1d4e961e9f7af95c5606e
farlee2121 <spencerf2@gmail.com>
2018-03-11 22:19:59 -0500

    Fill in basic TodoListAccessor methods and add transaction scope to manager test base to account for integration mode

---
+++ Tests.AccessorTests/TodoListAccessorTests.cs
@@ -0,0 +1,25 @@
+﻿using Accessors.DatabaseAccessors;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Test.NUnitExtensions;
+
+namespace Tests.AccessorTests
+{
+    [TestFixture_Prefixed(typeof(TodoListAccessor))]
+    public class TodoListAccessorTests : AccessorTestBase
+    {
+        TodoListAccessor accessor;
+        public override void OnInitialize()
+        {
+            accessor = new TodoListAccessor();
+        }
+
+        [Test]
+        public void GetTodoList()
+        {
+
+        }
+    }
+}
